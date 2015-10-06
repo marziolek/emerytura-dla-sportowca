04f29d1cdaec2c222d01f704bf8491d2b8bf8be4
Marcin Ziolek <mar.ziolek@gmail.com>
2015-10-06 09:34:36 +0200

    footer

--- Gruntfile.js
+++ Gruntfile.js
@@ -84,7 +84,7 @@ module.exports = function (grunt) {
       options: {
         port: 9000,
         // Change this to '0.0.0.0' to access the server from outside.
-        hostname: 'localhost',
+        hostname: '0.0.0.0',
         livereload: 35729
       },
       livereload: {
@@ -221,7 +221,6 @@ module.exports = function (grunt) {
         src: [
           '<%= yeoman.dist %>/scripts/{,*/}*.js',
           '<%= yeoman.dist %>/styles/{,*/}*.css',
-          '<%= yeoman.dist %>/images/**/*.{png,jpg,jpeg,gif,webp,svg}',
           '<%= yeoman.dist %>/fonts/*'
         ]
       }
@@ -231,7 +230,7 @@ module.exports = function (grunt) {
     // concat, minify and revision files. Creates configurations in memory so
     // additional tasks can operate on them
     useminPrepare: {
-      html: '<%= yeoman.app %>/index.html',
+      html: '<%= yeoman.app %>/**/*.html',
       options: {
         dest: '<%= yeoman.dist %>',
         flow: {
@@ -248,13 +247,11 @@ module.exports = function (grunt) {
 
     // Performs rewrites based on filerev and the useminPrepare configuration
     usemin: {
-      html: ['<%= yeoman.dist %>/{,*/}*.html'],
+      html: ['<%= yeoman.dist %>/**/*.html'],
       css: ['<%= yeoman.dist %>/styles/{,*/}*.css'],
       options: {
         assetsDirs: [
           '<%= yeoman.dist %>',
-          '<%= yeoman.dist %>/images',
-          '<%= yeoman.dist %>/images/quotes',
           '<%= yeoman.dist %>/styles'
         ]
       }
---
+++ app/phpMailing/sendmail.php
@@ -0,0 +1,41 @@
+<?php
+
+function spamcheck($field)
+{
+  //filter_var() sanitizes the e-mail
+  //address using FILTER_SANITIZE_EMAIL
+  $field=filter_var($field, FILTER_SANITIZE_EMAIL);
+
+  //filter_var() validates the e-mail
+  //address using FILTER_VALIDATE_EMAIL
+  if(filter_var($field, FILTER_VALIDATE_EMAIL))
+  {
+    return true;
+  }
+  else
+  {
+    return false;
+  }
+}
+
+function sendMail($toEmail, $fromEmail, $subject, $message)
+{
+  $validFromEmail = spamcheck($fromEmail);
+  if($validFromEmail)
+  {
+    mail($toEmail, $subject, $message, "From: $fromEmail");
+  }
+}
+
+$email = 'mar.ziolek@gmail.com';
+$fullname = isset($_POST['fullname']) ? $_POST['fullname'] : false;
+$phone = isset($_POST['phone']) ? $_POST['phone'] : false;
+$hour = isset($_POST['hour']) ? $_POST['hour'] : false;
+
+if($email != false)
+{
+  $yourEmail = $email;
+  $subject = 'Prośba o kontakt od '. $fullname;
+  $message = $fullname . ' prosi o kontakt pod numerem: '. $phone .' w godzinach: '. $hour;
+  $success = sendMail($email, $yourEmail, $subject, $message);
+}
